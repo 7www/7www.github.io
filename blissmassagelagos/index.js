ed81c57bf12dd40dff8c6952ddcc6485a28b0c4a
7www <50775884+7www@users.noreply.github.com>
2022-01-22 20:51:54 +0000

    hh

--- blissmassagelagos/index.js
+++ blissmassagelagos/index.js
@@ -2,9 +2,9 @@ let request = new XMLHttpRequest();
 request.open("GET","index.json", false);
 request.send(null);
 let jsonfile = JSON.parse(request.responseText);
-
-console.log(jsonfile);
-
+jsonfile = jsonfile[navigator.language.substring(0,2)]
+document.title = jsonfile['h1-header'] + " | " + jsonfile['h2-header'] 
+document.documentElement.lang = navigator.language;
 for (let key in jsonfile) {
     if (jsonfile.hasOwnProperty(key)) {
         console.log(key + " -> " + jsonfile[key]);
@@ -13,6 +13,9 @@ for (let key in jsonfile) {
         console.log(element.nodeName);
         if (element.nodeName.includes("IMG")){
             document.getElementById(key).src = jsonfile[key];        
+        }
+        if (element.id.includes("footer-button")){
+            document.getElementById(key).value = jsonfile[key];        
         }
         if (element.id.includes("header-social-links")||element.id.includes("footer-social-links")){
             document.getElementById(key).href = jsonfile[key];        
